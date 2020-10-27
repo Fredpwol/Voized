de54626083f050a20aaf971698c5829d5a4aafca
Fredpwol <cyborg290@gmail.com>
2020-10-27 19:55:17 +0100

    mock login user reducer

--- src/reducer/authReducer.js
+++ src/reducer/authReducer.js
@@ -1,13 +1,25 @@
 /* eslint-disable import/no-anonymous-default-export */
 
-const initialState = {token: null, username: null, password:null, email: null, isAuthenticated:false};
+import { LOGIN_USER } from "../actions/constants";
 
-export default (state=initialState, action, ) => {
-    switch (action.type) {
-        case 'SET_TOKEN':
-            break;
-    
-        default:
-            return state;
-    }
-}
+const initialState = {
+  token: null,
+  username: null,
+  password: null,
+  email: null,
+  isAuthenticated: false,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_USER:
+      return {
+        ...state,
+        username: action.payload.username,
+        password: action.payload.password,
+        isAuthenticated: true,
+      };
+    default:
+      return state;
+  }
+};

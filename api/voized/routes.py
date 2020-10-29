ead3ed46a4852da9ac2ceb95d063745e9321b0c8
Fredpwol <cyborg290@gmail.com>
2020-10-29 10:57:32 +0100

    created login route

--- api/voized/routes.py
+++ api/voized/routes.py
@@ -7,25 +7,38 @@ auth = HTTPBasicAuth()
 
 
 @auth.verify_password
-def verify_password(username, password):
+def verify_password(username_or_token, password):
+    user = User.verify_web_token(username_or_token)
+    if not user:
+        user = User.query.filter_by(username=username_or_token)
+        if not (user and user.validate_passowrd(password)):
+             return False
+    g.user = user
     return True
 
 @app.route('/user/login', methods = ["POST"])
 def login_user():
     if request.method == "POST":
         print(request.json)
-    username = request.json.get('username')
-    password = request.json.get('password')
-    return { "name":"fred" }, 200
+    username = request.get_json('username')
+    password = request.get_json('password')
+    if username is None or password is None:
+        return {"status": "error", "message":"Input a valid username or password!"}, 400
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return {"status": "error", "message":"Input a valid username or password!"}, 400
+    token = user.generate_web_token()
+    return jsonify({ "status" :'success',"username": username, "token": token, "exp_sec":259200}), 200
+
 
 @app.route("/user/register", methods = ["POST"])
 def register_user():
     try:
-        username = request.json.get("username")
-        password = request.json.get("password")
+        username = request.get_json("username")
+        password = request.get_json("password")
         email = request.json.get("email")
         if username is None or password is None:
-            return {"status": "error", "message":"Input a valid username or pasoword!"}, 400
+            return {"status": "error", "message":"Input a valid username or password!"}, 400
         if  User.query.filter_by(username = username).first():
             return {"status": "error", "message": "Username already taken."}, 409
         if User.query.filter_by(email = email).first():
@@ -36,4 +49,6 @@ def register_user():
         db.session.add(user)
         db.session.commit()
 
-        return jsonify({ "username": username, "token": token, "exp_sec":259200}), 201
+        return jsonify({ "status": "success","username": username, "token": token, "exp_sec":259200}), 201
+    except Exception as e:
+        return jsonify({"status": "error", "message": "An unknwon error occured!"})

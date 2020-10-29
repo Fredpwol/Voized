52b88d00e089a92d678e24e8733adbec3241fea4
Fredpwol <cyborg290@gmail.com>
2020-10-29 14:23:17 +0100

    increased expires seconds

--- api/voized/routes.py
+++ api/voized/routes.py
@@ -1,4 +1,4 @@
-from voized import app
+from voized import app, db
 from voized.models import User
 from flask import request, jsonify,make_response, g
 from flask_httpauth import HTTPBasicAuth
@@ -20,22 +20,22 @@ def verify_password(username_or_token, password):
 def login_user():
     if request.method == "POST":
         print(request.json)
-    username = request.get_json('username')
-    password = request.get_json('password')
+    username = request.json.get('username')
+    password = request.json.get('password')
     if username is None or password is None:
-        return {"status": "error", "message":"Input a valid username or password!"}, 400
-    user = User.query.filter_by(username=username).first()
-    if not user:
-        return {"status": "error", "message":"Input a valid username or password!"}, 400
-    token = user.generate_web_token()
-    return jsonify({ "status" :'success',"username": username, "token": token, "exp_sec":259200}), 200
+        return jsonify({"status": "error", "message":"Input a valid username or password!"}), 400
+    user = User.query.filter_by(username=username).first() or User.query.filter_by(email = username).first()
+    if (not user) or (not user.validate_password(password)):
+        return jsonify({"status": "error", "message":"Invaid username or password!"}), 400
+    token = user.generate_web_token(9999999999)
+    return jsonify({ "status" :'success',"username": user.username,"email":user.email, "token": token}), 200
 
 
 @app.route("/user/register", methods = ["POST"])
 def register_user():
     try:
-        username = request.get_json("username")
-        password = request.get_json("password")
+        username = request.json.get('username')
+        password = request.json.get('password')
         email = request.json.get("email")
         if username is None or password is None:
             return {"status": "error", "message":"Input a valid username or password!"}, 400
@@ -45,7 +45,7 @@ def register_user():
             return {"status": "error", "message": "Email already taken."}, 400
 
         user = User(username=username, password=password, email=email)
-        token = user.generate_web_token()
+        token = user.generate_web_token(9999999999)
         db.session.add(user)
         db.session.commit()
 

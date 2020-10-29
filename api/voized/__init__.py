880e79a0ff63b43e307453ad8943088b444aa1c1
Fredpwol <cyborg290@gmail.com>
2020-10-29 10:38:18 +0100

    added bcrypt

--- api/voized/__init__.py
+++ api/voized/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from .config import DevelopmentConfig
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:pandroid016@localhost:5432/voized_db"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+
+from voized import routes

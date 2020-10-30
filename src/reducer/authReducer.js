0fa98ca5a2146d74439df58a99d8dd18eedb15f2
Fredpwol <cyborg290@gmail.com>
2020-10-30 08:23:27 +0100

    used localStorage to save state pesistently

--- src/reducer/authReducer.js
+++ src/reducer/authReducer.js
@@ -1,24 +1,47 @@
 /* eslint-disable import/no-anonymous-default-export */
 
-import { LOGIN_USER } from "../actions/constants";
+import { LOGGING_IN, LOGIN_ERROR, LOGIN_USER } from "../actions/constants";
 
 const initialState = {
-  token: null,
-  username: null,
-  password: null,
-  email: null,
-  isAuthenticated: false,
+  token: localStorage.getItem("token"),
+  username: localStorage.getItem("username"),
+  password: localStorage.getItem("password"),
+  email: localStorage.getItem("email"),
+  isAuthenticated: localStorage.getItem("isAuthenticated"),
+  loginError:"",
+  SignupError:"",
+  loggingIn:false,
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case LOGIN_USER:
+      localStorage.setItem("token", action.payload.token)
+      localStorage.setItem("username", action.payload.username)
+      localStorage.setItem("password", action.payload.password)
+      localStorage.setItem("email", action.payload.email)
+      localStorage.setItem("isAuthenticated", true)
       return {
         ...state,
         username: action.payload.username,
         password: action.payload.password,
+        email: action.payload.email,
+        token: action.payload.token,
         isAuthenticated: true,
+        loggingIn:false
       };
+    
+    case LOGIN_ERROR:
+      return{
+        ...state,
+        loginError:action.payload.message,
+        loggingIn:false
+      }
+    case LOGGING_IN:
+      return{
+        ...state,
+        loggingIn:true
+      }
     default:
       return state;
   }

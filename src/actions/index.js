c3fee721a893895f3c99ae1a7b8860e3625592de
Fredpwol <cyborg290@gmail.com>
2020-10-30 08:22:54 +0100

    login user from backend

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,30 @@
-import { LOGIN_USER } from "./constants"
+import { LOGIN_ERROR, LOGIN_USER, LOGGING_IN } from "./constants";
 
-export const loginUser =  ({username, password}) => (
-    {
-        type:LOGIN_USER,
-        payload:{username, password}
-    }
-)
+export const loginUser = ({ username, password }) => {
+  return (disbatch) => {
+    disbatch({ type: LOGGING_IN });
+    fetch("/user/login", {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      }),
+      body: JSON.stringify({ username, password }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "success") {
+          disbatch({
+            payload: {
+              username: data.username,
+              email: data.email,
+              token: data.token,
+            },
+            type: LOGIN_USER,
+          });
+        } else {
+          disbatch({ type: LOGIN_ERROR, payload: { message: data.message } });
+        }
+      });
+  };
+};

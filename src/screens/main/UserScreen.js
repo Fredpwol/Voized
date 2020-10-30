18e8e098f3c02c0e3df0d36ab44f1fd32f7d2a7d
Fredpwol <cyborg290@gmail.com>
2020-10-30 15:34:41 +0100

    created user screen header

--- src/screens/main/UserScreen.js
+++ src/screens/main/UserScreen.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React from "react";
+import { Avatar, Typography, Button } from "antd";
+import { LogoutOutlined } from "@ant-design/icons";
+import { connect } from "react-redux";
+import { blue, presetPrimaryColors } from "@ant-design/colors";
 
 
-const UserScreen = () => {
-    return(
-        <div>
-            UserScreen
-        </div>
-    )
+import { getNameInitials } from "../../utils"
+
+const UserScreen = ({user:{userBgColor, username}}) => {
+  return (
+    <div>
+      <div className="profile-bg">
+        <Avatar
+          size={{ xs: 24, sm: 30, md: 90, lg: 130, xl: 160, xxl: 200 }}
+          style={{
+            bottom: "-15vh",
+            marginLeft: "15px",
+            fontSize: "15vh",
+            backgroundColor: presetPrimaryColors[userBgColor],
+          }}
+        >
+          {getNameInitials(username)}
+        </Avatar>
+        <Typography.Title style={{color:"#ffffff", marginLeft:"20%", marginBottom:"5px"}}>
+          {username}
+        </Typography.Title>
+      </div>
+      <div>
+      <Button type="primary" danger icon={<LogoutOutlined />} style={{right:10, position:"absolute"}} >
+      Logout
+    </Button>
+      </div>
+    </div>
+  );
 };
 
+const mapStateToProps = state => (
+  {
+    user: state.auth
+  }
+)
 
-
-export default UserScreen;
+export default connect(mapStateToProps,null)(UserScreen);

beb2f7e604484c9d5ff8179b681d0a38c1b223ca
Fredpwol <cyborg290@gmail.com>
2020-11-02 16:16:47 +0100

    added call models and modified rooms

--- api/voized/models.py
+++ api/voized/models.py
@@ -1,4 +1,5 @@
 import random
+import time
 from voized import db, bcrypt, app
 from flask_login import UserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired
@@ -7,6 +8,9 @@ friends = db.Table("contacts", db.Column("user", db.Integer, db.ForeignKey(
     "users.id")), db.Column("friend", db.Integer, db.ForeignKey("users.id")))
 groups = db.Table("groups", db.Column("user_id", db.Integer, db.ForeignKey(
     "users.id")), db.Column("room_id", db.Integer, db.ForeignKey("rooms.id")))
+group_calls = db.Table("group_calls", db.Column("room_id", db.Integer, db.ForeignKey(
+    "rooms.id")), db.Column("user_id", db.Integer, db.ForeignKey("users.id")))
+
 
 class User(db.Model):
     __tablename__ = "users"
@@ -19,7 +23,8 @@ class User(db.Model):
     profile_pic = db.Column(db.String(255), nullable=True)
     contacts = db.relationship("User", secondary=friends, primaryjoin=(
         friends.c.friend == id), secondaryjoin=(friends.c.user == id), lazy="dynamic")
-    rooms = db.relationship("Room", secondary=groups, backref=db.backref("members", lazy="dynamic"), lazy="dynamic") 
+    rooms = db.relationship("Room", secondary=groups, backref=db.backref(
+        "members", lazy="dynamic"), lazy="dynamic")
 
     def __init__(self, username, password, email):
         self.username = username
@@ -35,8 +40,8 @@ class User(db.Model):
             "email": self.email,
             "bg_color": self.bg_color,
             "profile_pic": self.profile_pic,
-            "contacts": [ friend.serialize for friend in self.contacts ],
-            "rooms": [ room.serialize for room in self.rooms ]
+            "contacts": [friend.serialize for friend in self.contacts],
+            "rooms": [room.serialize for room in self.rooms]
         }
 
     def validate_password(self, password):
@@ -54,13 +59,26 @@ class User(db.Model):
     def add_contact(self, user):
         if not self.is_contact(user):
             self.contacts.append(user)
-        
+
     def remove_contact(self, user):
         if self.is_contact(user):
             self.contacts.remove(user)
 
     def is_contact(self, user):
-        return self.contacts.filter( friends.c.friend == user.id).count() > 0
+        return self.contacts.filter(friends.c.friend == user.id).count() > 0
+
+    def in_room(self, room):
+        return self.rooms.filter(groups.c.room_id == room.id).count() > 0
+
+    def join_room(self, room):
+        if not self.in_room(room):
+            self.roooms.append(room)
+            room.no_user += 1
+
+    def leave_room(self, room):
+        if self.in_room(room):
+            self.roooms.append(room)
+            room.no_user -= 1
 
     @staticmethod
     def verify_web_token(token):
@@ -78,13 +96,25 @@ class User(db.Model):
         return f"User({self.username})"
 
 
-
 class Room(db.Model):
     __tablename__ = "rooms"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     group_image = db.Column(db.String(255), nullable=True)
-    no_users = db.Column(db.Integer, default=0, nullable=False)
+    category = db.Column(db.String(64), default="general", nullable=False)
+    description = db.Column(db.String, nullable=True)
+    created_at = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, name, category="general", description=None, group_image=None,):
+        self.name = name
+        self.category = category
+        self.description = description
+        self.group_image = group_image
+        self.created_at = time.time()
+
+    @property
+    def no_user(self):
+        return len(self.members)
 
     def serialize(self):
         return {
@@ -94,6 +124,34 @@ class Room(db.Model):
             "noUsers": self.no_users
         }
 
-
     def __repr__(self):
         return f"Room({self.name})"
+
+
+class CallMixins:
+    id = db.Column(db.Integer, primary_key=True)
+    at = db.Column(db.Integer, nullable=False)
+    duration = db.Column(db.Integer, nullable=True)
+    ended = db.Column(db.Integer, nullable=True)
+    status = db.Column(db.String, nullable=False)
+
+
+class Calls(db.Model, CallMixins):
+    __tablename__ = "calls"
+    caller = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    reciever = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+
+class GroupCalls(db.Model, CallMixins):
+    __tablename__ = "room_calls"
+    room = db.Column(db.Integer, db.ForeignKey("rooms.id"), nullable=False)
+    to = db.relationship("User", secondary=group_calls, backref=db.backref(
+        "room_calls", lazy="dynamic"), lazy="dynamic")
+
+
+class VoiceMails(db.Model, CallMixins):
+    __tablename__ = "voice_mails"
+
+    caller = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    reciever = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    mail = db.Column(db.String(255), nullable=True)

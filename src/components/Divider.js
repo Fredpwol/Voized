0d5e635e87102143e88803bb4a457c4be79c733c
Fredpwol <cyborg290@gmail.com>
2020-11-02 10:19:33 +0100

    created divider

---
+++ src/components/Divider.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Divider = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#ccd0d6",
+        height: "1px",
+      }}
+    ></div>
+  );
+};
+
+export default Divider;

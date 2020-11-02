655be30ceacc0bf9ca15d97adeda93a483753ddc
Fredpwol <cyborg290@gmail.com>
2020-11-02 10:22:29 +0100

    added header and search bar

--- src/screens/main/Contact.js
+++ src/screens/main/Contact.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Typography, Layout, Input } from "antd"
+
+
+import UserItem from '../../components/UserItem';
+import Divider from '../../components/Divider';
 
 
 const Contact = () => {
     return(
-        <div>
-            Contact
+        <div className="content-body">
+            <Layout.Header className="content-head">
+                <h1 className="title-text" >
+                    Contacts
+                </h1>
+                <Input.Search placeholder="Type in to Search..." enterButton style={{width:"40%"}} />
+            </Layout.Header>
+            <Divider />
+            <div style={{paddingTop:"10px"}}>
+                <UserItem />
+            </div>
         </div>
     )
 };

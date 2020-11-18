cb4342d13a8d8ba4c0d47510ee7c3537df9326ef
Fredpwol <cyborg290@gmail.com>
2020-11-18 07:33:49 +0100

    added callData Reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import appDataReducer from "./appDataReducer";
 import userDataReducer from "./userDataReducer";
+import CallReducer from "./callReducer";
 
 export default combineReducers({
     auth: authReducer,
     appData: appDataReducer,
-    userData: userDataReducer
+    userData: userDataReducer,
+    callData: CallReducer
 })

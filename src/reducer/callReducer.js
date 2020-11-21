c7228b8b18b0cbc3a3f812fd079276ea1ce9fc13
Fredpwol <cyborg290@gmail.com>
2020-11-21 22:29:11 +0100

    changed recipient dtype to array

--- src/reducer/callReducer.js
+++ src/reducer/callReducer.js
@@ -1,11 +1,11 @@
 import { RINGING, SET_OFFER, STOP_RINGING, SET_ANSWER, CLEAR_RECIPIENT, SET_PEER } from "../actions/constants";
 
-const initialState = {calls: [], onCall: false, ringing: false, peer:{}, timeElapsed: 0, recipient: {}, offer: {}, answer:{} }
+const initialState = {calls: [], onCall: false, ringing: false, peer:{}, timeElapsed: 0, recipient: [], offer: {}, answer:{} }
 
 export default function CallReducer(state=initialState, action){
     switch (action.type){
         case RINGING :
-            return { ...state, ringing: true, recipient: action.payload };
+            return { ...state, ringing: true, recipient: [...state.recipient, action.payload] };
         case SET_OFFER:
             return {...state, offer: action.payload}
         case STOP_RINGING:

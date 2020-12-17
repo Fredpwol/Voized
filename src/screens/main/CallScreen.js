74f4ca91b9626ed5dc27c044217363ab659d2eb6
Fredpwol <cyborg290@gmail.com>
2020-12-17 22:04:45 +0100

    reformat code

--- src/screens/main/CallScreen.js
+++ src/screens/main/CallScreen.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import { Modal, Avatar, Typography } from "antd";
 import { blue } from "@ant-design/colors";
-import { AudioMutedOutlined, PhoneFilled, PauseOutlined } from "@ant-design/icons";
+import {
+  AudioMutedOutlined,
+  PhoneFilled,
+  PauseOutlined,
+} from "@ant-design/icons";
 import { getNameInitials } from "../../utils";
 import { stopCall } from "../../utils/client";
 import RecordBtn from "../../assets/images/recording.svg";
 import Timer from "../../components/Timer";
 
 const CallScreen = ({ isvisible, caller, collapsed }) => {
-  const [start, setStart] = useState(new Date())
+  const [start, setStart] = useState(new Date());
   const [isFullscreen, setisFullscreen] = useState(true);
   return isFullscreen ? (
     <Modal
@@ -45,28 +49,59 @@ const CallScreen = ({ isvisible, caller, collapsed }) => {
           </div>
         </div>
         <div className="floating-actions">
-        <Timer start={start} style={{fontSize:"30px", color:"white",marginTop:"8px", paddingLeft:"15px", paddingRight:"15px"}} />
-        <div className="center-item floating-action-btn">
+          <Timer
+            start={start}
+            style={{
+              fontSize: "30px",
+              color: "white",
+              marginTop: "8px",
+              paddingLeft: "15px",
+              paddingRight: "15px",
+            }}
+          />
+          <div className="center-item floating-action-btn">
             <img src={RecordBtn} />
           </div>
-        <div className="center-item floating-action-btn">
+          <div className="center-item floating-action-btn">
             <PauseOutlined />
           </div>
-        <div className="center-item floating-action-btn" style={{ backgroundColor: "red" }}>
+          <div
+            className="center-item floating-action-btn"
+            style={{ backgroundColor: "red" }}
+          >
             <PhoneFilled onClick={() => stopCall()} />
           </div>
         </div>
       </div>
     </Modal>
-  ): ( isvisible ? (    
-  <div onClick={() => setisFullscreen(true)} style={{height: "80px", width:"100%", position:"absolute", backgroundColor:"lawngreen", bottom:0, display:"flex", paddingLeft:"80px", paddingTop:"18px"}}>
-  <Timer start={start} style={{fontSize:"30px", color:"white", marginLeft:(collapsed ? "80px" :"200px"), paddingRight:"15px"}} />
-  <div style={{marginLeft:"20%", color:"#ffffff"}}>
-    <h2 >Click to return to call</h2>
-  </div>
-</div>)
-:null
-  );
+  ) : isvisible ? (
+    <div
+      onClick={() => setisFullscreen(true)}
+      style={{
+        height: "80px",
+        width: "100%",
+        position: "absolute",
+        backgroundColor: "lawngreen",
+        bottom: 0,
+        display: "flex",
+        paddingLeft: "80px",
+        paddingTop: "18px",
+      }}
+    >
+      <Timer
+        start={start}
+        style={{
+          fontSize: "30px",
+          color: "white",
+          marginLeft: collapsed ? "80px" : "200px",
+          paddingRight: "15px",
+        }}
+      />
+      <div style={{ marginLeft: "20%", color: "#ffffff" }}>
+        <h2>Click to return to call</h2>
+      </div>
+    </div>
+  ) : null;
 };
 //Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
 //Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>

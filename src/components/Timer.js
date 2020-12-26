5a024c5d651e2a42e56aaa56d976df2be745ae83
Fredpwol <cyborg290@gmail.com>
2020-12-26 11:17:28 +0100

    restructured timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,41 +1,34 @@
-import React, { useEffect, useState } from 'react';
-
-
+import React, { useEffect, useState } from "react";
 
 const zFormat = (string) => {
-    let res = string.toString();
-    if (res.length < 2){
-        res = "0" + res;
-    }
-    return res;
-}
-
-const Timer = ({style, start}) => {
-    const [seconds, setSeconds] = useState(0);
-    const [minutes, setMinutes] = useState(0);
-    const [hours, setHours] = useState(0)
-
-
-   var myVar = setInterval(myTimer, 1000);
-
-function myTimer() {
-  var d = new Date(Date.now() - start);
-  setHours(d.getHours()-1)
-  setMinutes(d.getMinutes())
-  setSeconds(d.getSeconds())
-}
-
-function myStopFunction() {
-  clearInterval(myVar);
-} 
-
-    return(
-        <div style={style}>
-            {`${zFormat(hours)}:${zFormat(minutes)}:${zFormat(seconds)}`}
-        </div>
-    )
+  let res = string.toString();
+  if (res.length < 2) {
+    res = "0" + res;
+  }
+  return res;
 };
 
+const Timer = ({ style, start }) => {
 
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHours] = useState(0);
+
+  useEffect( () => {
+    let myVar = setInterval(() => {
+      let d = new Date(Date.now() - start);
+      setHours(d.getHours() - 1);
+      setMinutes(d.getMinutes());
+      setSeconds(d.getSeconds());
+      }, 1000);
+    return clearInterval(myVar)
+  } , [start])
+
+  return (
+    <div style={style}>
+      {`${zFormat(hours)}:${zFormat(minutes)}:${zFormat(seconds)}`}
+    </div>
+  );
+};
 
 export default Timer;

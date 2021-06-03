0dcee4073b7a485e66878c671a4f511d28d7fd61
Fredpwol <cyborg290@gmail.com>
2021-06-03 17:43:13 +0100

    cors headers

--- api/voized/__init__.py
+++ api/voized/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from .config import DevelopmentConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:pandroid016@localhost:5432/voized_db"
 db = SQLAlchemy(app)

53c67ff3bf9a093a735982e6a9a9103fecb3714b
Fredpwol <cyborg290@gmail.com>
2021-06-03 17:49:42 +0100

    fixed timer component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,34 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-const zFormat = (string) => {
-  let res = string.toString();
-  if (res.length < 2) {
-    res = "0" + res;
+class Timer extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {seconds: 0, minutes: 0, hours: 0};
   }
-  return res;
-};
 
-const Timer = ({ style, start }) => {
-
-    const [seconds, setSeconds] = useState(0);
-    const [minutes, setMinutes] = useState(0);
-    const [hours, setHours] = useState(0);
-
-  useEffect( () => {
-    let myVar = setInterval(() => {
-      let d = new Date(Date.now() - start);
-      setHours(d.getHours() - 1);
-      setMinutes(d.getMinutes());
-      setSeconds(d.getSeconds());
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      let d = new Date(Date.now() - this.props.start);
+      this.setState({ hours: d.getHours() - 1, minutes: d.getMinutes(), seconds: d.getSeconds()})
       }, 1000);
-    return clearInterval(myVar)
-  } , [start])
+  }
 
-  return (
-    <div style={style}>
-      {`${zFormat(hours)}:${zFormat(minutes)}:${zFormat(seconds)}`}
+  componentWillUnmount(){
+    clearInterval(this.timer);
+  }
+  
+  zFormat = (string) => {
+    let res = string.toString();
+    if (res.length < 2) {
+      res = "0" + res;
+    }
+    return res;
+  };
+
+  render(){
+    return (
+      <div style={this.props.style}>
+      {`${this.zFormat(this.state.hours)}:${this.zFormat(this.state.minutes)}:${this.zFormat(this.state.seconds)}`}
     </div>
-  );
-};
-
+    )
+  }
+}
 export default Timer;

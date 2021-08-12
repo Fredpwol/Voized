3530cca625359371e71c7da370d11c34a65d3f5f
Fredpwol <fredpwol@gmail.com>
2021-08-12 07:46:39 +0100

    made email and username indexable

--- api/voized/models.py
+++ api/voized/models.py
@@ -16,8 +16,8 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=False, unique=True)
+    username = db.Column(db.String(30), nullable=False, unique=True, index=True)
+    email = db.Column(db.String(255), nullable=False, unique=True, index=True)
     password = db.Column(db.String(255), nullable=False)
     bg_color = db.Column(db.String(50), nullable=True, default="orange")
     profile_pic = db.Column(db.String(255), nullable=True)
